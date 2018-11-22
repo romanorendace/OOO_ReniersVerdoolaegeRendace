425d8cee290fe3530b50a05815a2857accf502fa
Brekke10000 <brend.reniers@gmail.com>
2018-11-22 17:39:05 +0100

    h

--- ZelfEvaluatieApp/src/application/Main.java
+++ ZelfEvaluatieApp/src/application/Main.java
@@ -14,39 +14,39 @@ import view.panels.QuestionOverviewPane;
 import view.panels.TestPane;
 
 public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
+    @Override
+    public void start(Stage primaryStage) {
 
-		try {
-			QuestionOverviewPane questionOverviewPane = new QuestionOverviewPane();
-			QuestionDetailPane questionDetailPane = new QuestionDetailPane();
+        try {
+            QuestionOverviewPane questionOverviewPane = new QuestionOverviewPane();
+            QuestionDetailPane questionDetailPane = new QuestionDetailPane();
 
-			CategoryOverviewPane categoryOverviewPanel = new CategoryOverviewPane();
-			CategoryDetailPane categoryDetailPanel = new CategoryDetailPane();
+            CategoryOverviewPane categoryOverviewPanel = new CategoryOverviewPane();
+            CategoryDetailPane categoryDetailPanel = new CategoryDetailPane();
 
-			TestPane testPane = new TestPane();
-			MessagePane messagePane = new MessagePane();
+            TestPane testPane = new TestPane();
+            MessagePane messagePane = new MessagePane();
 
-			Group root = new Group();
+            Group root = new Group();
 
-			Scene scene = new Scene(root, 500, 400);
+            Scene scene = new Scene(root, 500, 400);
 
 
-			BorderPane borderPane = new AssesMainPane(messagePane, categoryOverviewPanel, questionOverviewPane);
-			borderPane.prefHeightProperty().bind(scene.heightProperty());
-			borderPane.prefWidthProperty().bind(scene.widthProperty());
+            BorderPane borderPane = new AssesMainPane(messagePane, categoryOverviewPanel, questionOverviewPane);
+            borderPane.prefHeightProperty().bind(scene.heightProperty());
+            borderPane.prefWidthProperty().bind(scene.widthProperty());
 
-			root.getChildren().add(borderPane);
-			primaryStage.setScene(scene);
-			primaryStage.sizeToScene();
+            root.getChildren().add(borderPane);
+            primaryStage.setScene(scene);
+            primaryStage.sizeToScene();
 
-			primaryStage.show();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	public static void main(String[] args) {
-		launch(args);
-	}
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
